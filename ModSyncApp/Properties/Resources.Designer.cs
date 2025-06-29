07145a3e1ef86d36d6e12e3be2b46b733af3db03
WillDoggo <cswillseph@gmail.com>
2025-06-29 17:04:15 -0400

    changing main project name to ModSync

--- ModSyncApp/Properties/Resources.Designer.cs
+++ ModSyncApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ModSyncApp.Properties {
+namespace ModSync.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ModSyncApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModSyncApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModSync.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
